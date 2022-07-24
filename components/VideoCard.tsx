501856100d6f8274fa48e75579466ef292b7aceb
btokmach <btokmach@gmail.com>
2022-07-24 17:37:11 +0300

    added upload functionality

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -67,7 +67,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           onMouseEnter={() => setIsHover(true)}
           onMouseLeave={() => setIsHover(false)}
           className="rounded-3xl">
-          <Link href="/">
+          <Link href={`/detail/${post._id}`}>
             <video
               ref={videoRef}
               loop
