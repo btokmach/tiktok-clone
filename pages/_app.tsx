830466328004c9476040becdad063ad90d025ce9
btokmach <btokmach@gmail.com>
2022-07-31 00:37:32 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Head from 'next/head';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
@@ -18,6 +19,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+      <Head>
+        <title>TikTik</title>
+      </Head>
       <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
         <Navbar />
         <div className="flex gap-6 md:gap-20">
